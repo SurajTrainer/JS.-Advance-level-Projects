2257d936beec830c51b8de582036d3e738769045
Suraj Singh <139257270+SurajTrainer@users.noreply.github.com>
2024-01-23 17:06:07 +0530

    Bike landing page..
    a small webside about the bike landing page...

---
+++ URL Landing page/script.js
@@ -0,0 +1,8 @@
+function imgslider(anything) {
+    document.querySelector('.bike').src = anything;
+}
+
+function changeBgcolor(color) {
+    let bgchange = document.querySelector('.sec');
+    bgchange.style.background = color;
+}
