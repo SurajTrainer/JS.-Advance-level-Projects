c6df867e23338560e00ce831f296ab98a1b1fd04
Suraj Singh <139257270+SurajTrainer@users.noreply.github.com>
2024-01-25 13:32:29 +0530

    landing page for PUMA shooes..
    here i have used html css and javascript for creating this webside and this is also responsive...

---
+++ product landing page/main.js
@@ -0,0 +1,8 @@
+//  for toggle..
+
+const hamburger = document.getElementById('hamburger');
+const navlist = document.getElementById('navlist');
+
+hamburger.addEventListener('click', ()=> {
+    navlist.classList.toggle('navlist-active');
+})
